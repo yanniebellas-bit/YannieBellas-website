5563468ff217f651a87224847b7dcee37b41c4a1
YANNIE B. BELLAS <YBBELLAS@Complab2.vsuisabel>
2025-12-04 11:05:28 +0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('action-button');
+    const message = document.getElementById('welcome-message');
+
+    button.addEventListener('click', () => {
+       
+        message.textContent = "Success! JavaScript is fully functional on the deployed site!";
+        button.style.backgroundColor = '#ffc107';
+        button.textContent = 'JS Verified';
+    });
+});
